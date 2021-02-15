c7498c56c872a62cf7ef60ab37c0e3fd444bcabf
aecio-prado <aecioprado@gmail.com>
2021-02-15 20:06:17 -0300

    instaciacao do objeto cliente

--- src/estacionamento/EstacionamentoApp.java
+++ src/estacionamento/EstacionamentoApp.java
@@ -3,9 +3,10 @@ package estacionamento;
 public class EstacionamentoApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
+
 		Cliente c = new Cliente();
+		
+		
 		
 
 	}
